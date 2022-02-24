18d5308bbfdb48f175f8fdfea34b164584593df2
codewithdnd <100120285+codewithdnd@users.noreply.github.com>
2022-02-24 20:31:21 +0530

    Add files via upload

---
+++ 07_array.c
@@ -0,0 +1,15 @@
+#include <stdio.h> 
+#include <conio.h>
+ 
+int main() 
+{ 
+    int i; 
+    int arr[5] = {10,20,30,40,50}; 
+    
+    for (i=0;i<5;i++) { 
+        printf("value of arr[%d] is %d \n", i, arr[i]); 
+    } 
+    
+    return 0;
+    getch();
+} 
